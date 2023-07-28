be95207937cd7eefd64f7ba6bfd178e0ad0e23b0
Aparajit Garg <aparajit.garg06@gmail.com>
2023-07-28 22:00:20 +0530

    added local storage to save notes data

--- Notes-app/src/script.js
+++ Notes-app/src/script.js
@@ -4,15 +4,41 @@ let noteList = []
 const addBtn = document.getElementById('addNoteBtn')
 const notesList = document.getElementById('notes_list')
 // noteList.push(notesList.childrenElement)
+
+notesList.innerHTML = localStorage.getItem('notes')
 console.log(notesList)
 
-const deleteNote = (e) => {
-    console.log(e.target.parentElement)
-    notesList.removeChild(e.target.parentElement)
+
+const updateLocalStorage = () => {
+    localStorage.setItem('notes', notesList.innerHTML)
 }
+
+const noteClicked = (e) => {
+    console.log(e.target.tagName)
+    switch (e.target.tagName) {
+        case 'P': {
+            const notes = document.querySelectorAll('.individual_note');
+            notes.forEach(nt => {
+                nt.onkeyup = function () {
+                    updateLocalStorage()
+                }
+            })
+            break;
+        }
+        case 'IMG': {
+            notesList.removeChild(e.target)
+            updateLocalStorage()
+            break;
+        }
+        default:
+            alert("Some issue with onClick listener");
+    }
+}
+
 
 const addNote = () => {
     const newElement = document.createElement('li')
+    newElement.className = 'individual_note'
     const inputElement = document.createElement('p')
     inputElement.contentEditable = true
     const img = document.createElement('img')
@@ -21,7 +47,9 @@ const addNote = () => {
     newElement.appendChild(img)
     notesList.appendChild(newElement)
 
-    img.addEventListener('click', deleteNote)
+    newElement.addEventListener('click', noteClicked)
+
+    localStorage.setItem('notes', notesList.innerHTML)
 }
 
 
